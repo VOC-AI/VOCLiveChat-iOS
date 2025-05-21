a884db9f6707d12763df4ab13b74d17f69f4ff3e
Boyuan Gao <anti2moron@gmail.com>
2025-05-21 16:00:13 +0800

    chore: pub script

--- publish.sh
+++ publish.sh
@@ -4,8 +4,4 @@ ver=`cat VocLiveChatFramework.podspec| grep 's.version ' |grep -oE '[0-9]+\.[0-9
 echo $ver
 git tag $ver
 git push --tags
-rm -rf ./VocLiveChatFramework/
-mkdir -p VocLiveChatFramework
-cp -R ./Assets/ VocLiveChatFramework/Assets
-cp -R ./Classes/ VocLiveChatFramework/Classes
 pod repo push SPEC_REPO *.podspec --verbose --use-libraries --allow-warnings
