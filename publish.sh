98ea352c0a2128812ac4a9398b0106140faa7d2b
Boyuan Gao <anti2moron@gmail.com>
2025-05-21 15:40:27 +0800

    chore: pub script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 git pull origin main
+ver=`cat VocLiveChatFramework.podspec| grep 's.version ' |grep -oE '[0-9]+\.[0-9]+\.[0-9]+'`
+echo $ver
+git tag $ver
+git push --tags
 rm -rf ./VocLiveChatFramework/
 mkdir -p VocLiveChatFramework
 cp -R ./Assets/ VocLiveChatFramework/Assets
