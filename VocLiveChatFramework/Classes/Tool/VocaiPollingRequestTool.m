88ef7074469e74836c81bd0f6853ad84f9587b47
Boyuan Gao <anti2moron@gmail.com>
2025-06-13 12:46:05 +0800

    fix

--- VocLiveChatFramework/Classes/Tool/VocaiPollingRequestTool.m
+++ VocLiveChatFramework/Classes/Tool/VocaiPollingRequestTool.m
@@ -32,17 +32,17 @@
     self.successBlock = success;
     self.failureBlock = failure;
     
-    // 创建 GCD 定时器
+    // Create GCD timer
     self.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0));
     dispatch_source_set_timer(self.timer, dispatch_time(DISPATCH_TIME_NOW, 0), interval * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
     
-    // 设置定时器回调
+    // Set timer callback
     __weak typeof(self) weakSelf = self;
     dispatch_source_set_event_handler(self.timer, ^{
         [weakSelf sendRequest];
     });
     
-    // 启动定时器
+    // Start timer
     dispatch_resume(self.timer);
 }
 
