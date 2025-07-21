08e7dd10fcebc3ab16668d55f9c831497be8fd26
Boyuan Gao <anti2moron@gmail.com>
2025-07-21 17:15:23 +0800

    fix: crash

--- VocLiveChatFramework.podspec
+++ VocLiveChatFramework.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'VocLiveChatFramework'
-  s.version          = '1.6.0'
+  s.version          = '1.6.1'
   s.summary          = 'VOC AI LiveChat iOS SDK'
 
 # This description is used to generate tags and improve search results.
--- VocLiveChatFramework/Classes/Tool/VocaiMessageCenter.m
+++ VocLiveChatFramework/Classes/Tool/VocaiMessageCenter.m
@@ -84,7 +84,7 @@
     if(!chatId) {
         cId = @"0";
     }
-    NSDictionary* params = @{@"userId": self.params.userId};
+    NSDictionary* params = self.params.userId ? @{@"userId": self.params.userId} : @{};
     [networkTool requestWithMethod:VocaiRequestMethodPOST URLString:url parameters:params
      success:^(id responseObject) {
         BOOL hasNewMessage = [self parseUnreadCountFromResponse:responseObject];
